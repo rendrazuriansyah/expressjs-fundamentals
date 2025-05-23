b4b08d0c4295ebf371635a9697af49a818e3a61e
Rendra Zuriansyah <rendra.zuriansyah.dev@gmail.com>
2025-05-24 00:59:09 +0700

    chore(app): configure basic root handler - add request and response parameters to root middleware - log incoming request headers for debugging - send a simple response to terminate the request

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.use(() => {
-  console.log('Middleware running');
+app.use((req, res) => {
+  console.log({ message: 'ini request' }, req.headers);
+  res.send('ini response');
 });
 
 app.listen(3000, () => {
