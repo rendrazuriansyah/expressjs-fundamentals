bc03584afe371ce752de7e823c3fe91bd24b4619
Rendra Zuriansyah <rendra.zuriansyah.dev@gmail.com>
2025-05-24 01:49:47 +0700

    feat(routes): add initial api routes - implement basic homepage route - add GET and POST endpoints for cats - include GET endpoint for dogs - replace generic catch-all with 404 not found handler

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const app = express();
 
+// app.use((req, res) => {
+//   console.log({ message: 'ini request' }, req.headers);
+//   res.send('ini response');
+// });
+
+app.get('/', (req, res) => {
+  res.send('homepage');
+});
+
+app.get('/cats', (req, res) => {
+  res.send('meow: get');
+});
+
+app.post('/cats', (req, res) => {
+  res.send('meow: post');
+});
+
+app.get('/dogs', (req, res) => {
+  res.send('woof');
+});
+
 app.use((req, res) => {
-  console.log({ message: 'ini request' }, req.headers);
-  res.send('ini response');
+  res.status(404).send('Not Found');
 });
 
 app.listen(3000, () => {
